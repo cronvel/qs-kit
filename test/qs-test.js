75e6943a4d46817208958c83eb8e5edac48fe005
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-04-14 15:09:30 +0200

    Parser breaking changes: 'rightHandSideBrackets' -> 'bracket' option, 'autoPush' option allow array creation from 'array=1&array=2&array=3', 'keyPath' option allow object/array path as key with the dot notation and the bracket notation, e.g. 'k1.k2[1]=value' will produce this object: {k1:{k2:[undefined,'value']}}

--- test/qs-test.js
+++ test/qs-test.js
@@ -41,7 +41,7 @@ var expect = require( 'expect.js' ) ;
 
 describe( "Parse" , function() {
 	
-	it( "Scalar usage" , function() {
+	it( "Scalar" , function() {
 		expect( qs.parse( "k=v" ) ).to.eql( { k: "v" } ) ;
 		expect( qs.parse( "key=value" ) ).to.eql( { key: "value" } ) ;
 		expect( qs.parse( "a=1" ) ).to.eql( { a: 1 } ) ;
@@ -52,13 +52,44 @@ describe( "Parse" , function() {
 	it( "String encoded" , function() {
 		expect( qs.parse( "key=some%20value" ) ).to.eql( { key: "some value" } ) ;
 	} ) ;
+	
+	it( "Array 'autoPush' option" , function() {
+		options = { autoPush: true } ;
+		expect( qs.parse( "key=one&key=two&key=three" ) ).to.eql( { key: 'three' } ) ;
+		expect( qs.parse( "key=one&key=two&key=three" , options ) ).to.eql( { key: [ 'one' , 'two' , 'three' ] } ) ;
+	} ) ;
+	
+	it( "Array with the 'brackets' option" , function() {
+		var options = { brackets: true } ;
+		expect( qs.parse( "key=[one,two,three]" ) ).to.eql( { key: "[one,two,three]" } ) ;
+		expect( qs.parse( "key=[one,two,three]" , options ) ).to.eql( { key: [ 'one' , 'two' , 'three' ] } ) ;
+	} ) ;
+	
+	it( "Array 'keyPath' option" , function() {
+		options = { keyPath: true } ;
+		expect( qs.parse( "a[0]=val&a[1]=val2" ) ).to.eql( { "a[0]": 'val' , "a[1]": 'val2' } ) ;
+		expect( qs.parse( "a[0]=val&a[1]=val2" , options ) ).to.eql( { a: [ 'val' , 'val2' ] } ) ;
+	} ) ;
+	
+	it( "Object 'keyPath' option" , function() {
+		options = { keyPath: true } ;
+		expect( qs.parse( "a.b=val" ) ).to.eql( { "a.b": 'val' } ) ;
+		expect( qs.parse( "a.b=val" , options ) ).to.eql( { a: { b: 'val' } } ) ;
+	} ) ;
+	
+	it( "'keyPath' and 'autoPush' option" , function() {
+		expect( qs.parse( "a.b=val&a.b=val2" ) ).to.eql( { "a.b": "val2" } ) ;
+		expect( qs.parse( "a.b=val&a.b=val2" , { keyPath: true } ) ).to.eql( { a: { b: "val2" } } ) ;
+		expect( qs.parse( "a.b=val&a.b=val2" , { autoPush: true } ) ).to.eql( { "a.b": [ "val" , "val2" ] } ) ;
+		expect( qs.parse( "a.b=val&a.b=val2" , { keyPath: true , autoPush: true } ) ).to.eql( { a: { b: [ "val" , "val2" ] } } ) ;
+	} ) ;
 } ) ;
 
 
 
 describe( "Stringify" , function() {
 	
-	it( "Scalar usage" ) ;
+	it( "Scalar" ) ;
 	it( "String encoded" ) ;
 } ) ;
 
@@ -66,7 +97,7 @@ describe( "Stringify" , function() {
 
 describe( "Historical bugs" , function() {
 	
-	it( "Bad query string: =one=two" , function() {
+	it( "Bad query string" , function() {
 		// should throw
 		expect( function() { qs.parse( "=one=two" ) ; } ).to.throwException() ;
 	} ) ;
