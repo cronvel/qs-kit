ed438ab58794246f569213ce3648a8cfffb7ca7e
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2018-09-14 14:59:17 +0200

    Dependency upgraded

--- test/qs-test.js
+++ test/qs-test.js
@@ -1,20 +1,20 @@
 /*
 	Query String Kit
-	
-	Copyright (c) 2014 - 2016 Cédric Ronvel
-	
+
+	Copyright (c) 2014 - 2018 Cédric Ronvel
+
 	The MIT License (MIT)
-	
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-	
+
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,7 +29,6 @@
 
 
 var qs = require( '../lib/qs.js' ) ;
-var expect = require( 'expect.js' ) ;
 
 
 
@@ -39,55 +38,60 @@ var expect = require( 'expect.js' ) ;
 
 
 
-describe( "Parse" , function() {
+describe( "Parse" , () => {
 	
-	it( "Scalar" , function() {
-		expect( qs.parse( "k=v" ) ).to.eql( { k: "v" } ) ;
-		expect( qs.parse( "key=value" ) ).to.eql( { key: "value" } ) ;
-		expect( qs.parse( "a=1" ) ).to.eql( { a: 1 } ) ;
-		expect( qs.parse( "key=value&a=1" ) ).to.eql( { key: "value" , a: 1 } ) ;
-		expect( qs.parse( "key=value&a=1&b=2&c=string" ) ).to.eql( { key: "value" , a: 1 , b: 2 , c: "string" } ) ;
+	it( "Scalar" , () => {
+		expect( qs.parse( "k=v" ) ).to.equal( { k: "v" } ) ;
+		expect( qs.parse( "key=value" ) ).to.equal( { key: "value" } ) ;
+		expect( qs.parse( "a=1" ) ).to.equal( { a: "1" } ) ;
+		expect( qs.parse( "key=value&a=1" ) ).to.equal( { key: "value" , a: "1" } ) ;
+		expect( qs.parse( "key=value&a=1&b=2&c=string" ) ).to.equal( { key: "value" , a: "1" , b: "2" , c: "string" } ) ;
 	} ) ;
 	
-	it( "String encoded" , function() {
-		expect( qs.parse( "key=some%20value" ) ).to.eql( { key: "some value" } ) ;
+	it( "String encoded" , () => {
+		expect( qs.parse( "key=some%20value" ) ).to.equal( { key: "some value" } ) ;
 	} ) ;
 	
-	it( "Array 'autoPush' option" , function() {
+	it( "Array 'autoPush' option" , () => {
 		options = { autoPush: true } ;
-		expect( qs.parse( "key=one&key=two&key=three" ) ).to.eql( { key: 'three' } ) ;
-		expect( qs.parse( "key=one&key=two&key=three" , options ) ).to.eql( { key: [ 'one' , 'two' , 'three' ] } ) ;
+		expect( qs.parse( "key=one&key=two&key=three" ) ).to.equal( { key: 'three' } ) ;
+		expect( qs.parse( "key=one&key=two&key=three" , options ) ).to.equal( { key: [ 'one' , 'two' , 'three' ] } ) ;
 	} ) ;
 	
-	it( "Array with the 'brackets' option" , function() {
+	it( "Array with the 'brackets' option" , () => {
 		var options = { brackets: true } ;
-		expect( qs.parse( "key=[one,two,three]" ) ).to.eql( { key: "[one,two,three]" } ) ;
-		expect( qs.parse( "key=[one,two,three]" , options ) ).to.eql( { key: [ 'one' , 'two' , 'three' ] } ) ;
+		expect( qs.parse( "key=[one,two,three]" ) ).to.equal( { key: "[one,two,three]" } ) ;
+		expect( qs.parse( "key=[one,two,three]" , options ) ).to.equal( { key: [ 'one' , 'two' , 'three' ] } ) ;
 	} ) ;
 	
-	it( "Array 'keyPath' option" , function() {
+	it( "Array 'keyPath' option" , () => {
 		options = { keyPath: true } ;
-		expect( qs.parse( "a[0]=val&a[1]=val2" ) ).to.eql( { "a[0]": 'val' , "a[1]": 'val2' } ) ;
-		expect( qs.parse( "a[0]=val&a[1]=val2" , options ) ).to.eql( { a: [ 'val' , 'val2' ] } ) ;
+		expect( qs.parse( "a[0]=val&a[1]=val2" ) ).to.equal( { "a[0]": 'val' , "a[1]": 'val2' } ) ;
+		expect( qs.parse( "a[0]=val&a[1]=val2" , options ) ).to.equal( { a: [ 'val' , 'val2' ] } ) ;
 	} ) ;
 	
-	it( "Object 'keyPath' option" , function() {
+	it( "Object 'keyPath' option" , () => {
 		options = { keyPath: true } ;
-		expect( qs.parse( "a.b=val" ) ).to.eql( { "a.b": 'val' } ) ;
-		expect( qs.parse( "a.b=val" , options ) ).to.eql( { a: { b: 'val' } } ) ;
+		expect( qs.parse( "a.b=val" ) ).to.equal( { "a.b": 'val' } ) ;
+		expect( qs.parse( "a.b=val" , options ) ).to.equal( { a: { b: 'val' } } ) ;
 	} ) ;
 	
-	it( "'keyPath' and 'autoPush' option" , function() {
-		expect( qs.parse( "a.b=val&a.b=val2" ) ).to.eql( { "a.b": "val2" } ) ;
-		expect( qs.parse( "a.b=val&a.b=val2" , { keyPath: true } ) ).to.eql( { a: { b: "val2" } } ) ;
-		expect( qs.parse( "a.b=val&a.b=val2" , { autoPush: true } ) ).to.eql( { "a.b": [ "val" , "val2" ] } ) ;
-		expect( qs.parse( "a.b=val&a.b=val2" , { keyPath: true , autoPush: true } ) ).to.eql( { a: { b: [ "val" , "val2" ] } } ) ;
+	it( "'keyPath' and 'autoPush' option" , () => {
+		expect( qs.parse( "a.b=val&a.b=val2" ) ).to.equal( { "a.b": "val2" } ) ;
+		expect( qs.parse( "a.b=val&a.b=val2" , { keyPath: true } ) ).to.equal( { a: { b: "val2" } } ) ;
+		expect( qs.parse( "a.b=val&a.b=val2" , { autoPush: true } ) ).to.equal( { "a.b": [ "val" , "val2" ] } ) ;
+		expect( qs.parse( "a.b=val&a.b=val2" , { keyPath: true , autoPush: true } ) ).to.equal( { a: { b: [ "val" , "val2" ] } } ) ;
 	} ) ;
 } ) ;
 
 
 
-describe( "Stringify" , function() {
+describe( "Stringify" , () => {
+	
+	it( "should stringify an object" , () => {
+		var object = { firstName: "Joe" , lastName: "Doe" } ;
+		expect( qs.stringify( object ) ).to.be( "firstName=Joe&lastName=Doe" ) ;
+	} ) ;
 	
 	it( "Scalar" ) ;
 	it( "String encoded" ) ;
@@ -95,11 +99,11 @@ describe( "Stringify" , function() {
 
 
 
-describe( "Historical bugs" , function() {
+describe( "Historical bugs" , () => {
 	
-	it( "Bad query string" , function() {
+	it( "Bad query string" , () => {
 		// should throw
-		expect( function() { qs.parse( "=one=two" ) ; } ).to.throwException() ;
+		expect( () => qs.parse( "=one=two" ) ).to.throw() ;
 	} ) ;
 } ) ;
 
